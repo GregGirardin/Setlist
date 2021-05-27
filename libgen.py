830cc60da955182d9f041db1ddee1545f1bd4712
Greg Girardin <girardin1972@hotmail.com>
2021-05-26 23:00:48 -0400

    a

--- libgen.py
+++ libgen.py
@@ -7,60 +7,54 @@
 
 import os, glob, json
 
-class Song( object ):
-  def __init__( self, fileName, songName ):
-    self.fileName = fileName
-    self.songName = songName
+llf = "../lyricLibrary.json"
 
-songLibrary = []
+localLibrary = {}
 re = "*.txt"
 matchList = glob.glob( re )
 
 for fName in matchList:
-  # Parse song txts for Name.
   sf = open( fName, "r" )
   fLines = sf.readlines()
   sf.close()
 
   if len( fLines ) > 0:
-    songName = fLines[ 0 ].rstrip()
-    songLibrary.append( Song( fName, songName ) )
+    newSong = {}
+    newSong[ "name" ] = fLines[ 0 ].rstrip()
+    newSong[ "artist" ] = ""
+    newSong[ "key" ] = ""
+    id = newSong[ "name" ] + "." + newSong[ "artist" ]
+    id = id.replace( " ", "_" )
+    id = id.replace( "'", "_" )
+    newSong[ "id" ] = id
+    lyrics = ""
+    for l in fLines[ 2 : ]: # Skip title line and the next empty line
+      lyrics += l
+    newSong[ "lyrics" ] = lyrics 
+    localLibrary[ id ] = newSong
 
-def getKey( item ):
-  return item.fileName
-
-songLibrary.sort( key=getKey )
-
-if len( songLibrary ) == 0:
+if len( localLibrary ) == 0:
   print( "No songs in local directory." )
-  exit()
+else:
+  # see if lyricLibrary.json exists. If so just merge so we don't lose any additional 
+  # song information like key, Artist, etc we may have added.
+  if os.path.isfile( llf ):
+    print( "Merging to " + llf )
+    with open( llf ) as jFile:
+      existingLibrary = json.load( jFile )
+      newCount = 0
+      for key in localLibrary:
+        if key not in existingLibrary:
+          newCount += 1
+          existingLibrary[ key ] = localLibrary[ key ]
 
-print( "Creating lyricLibrary.json" )
-
-library = {}
-output = ""
-for s in songLibrary:
-  sName = s.fileName
-  if not os.path.exists( sName ):
-    continue
-    
-  sf = open( sName, "r" )
-  fLines = sf.readlines()
-  sf.close()
-  libSong = {}
-  libSong[ "name" ] = s.songName
-  libSong[ "artist" ] = "Artist"
-  lyrics = ""# "<p style='font-family:verdana'>";
-  for l in fLines[ 2 : ]: # prune the first two lines. The song name and space below.
-    lyrics += l
-  #lyrics += "</p>";
-
-  libSong[ "lyrics" ] = lyrics; # skip title and following space
-  id = ( libSong[ "name" ] + "." + libSong[ "artist" ] )
-  id = id.replace( " ", "_" );
-  id = id.replace( "'", "_" );
-  libSong[ "id" ] = id
-  library[ id ] = libSong
-
-  with open( "lyricLibrary.json", 'w' ) as f:
-    json.dump( library, f, indent=2 )  
+      if newCount:
+        with open( llf, 'w' ) as f:
+          json.dump( existingLibrary, f, indent=2 )  
+        print( "added " + str( newCount ) + " songs." )
+      else:
+        print( "No changes." )
+  else:
+    print( "Creating " + llf )
+    with open( llf, 'w' ) as f:
+      json.dump( localLibrary, f, indent=2 )  
