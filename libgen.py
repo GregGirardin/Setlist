7305ed546b1fe3b607459d0c9251d1c4e41fe89c
Greg Girardin <girardin1972@hotmail.com>
2021-05-29 20:55:38 -0400

    fix CR

--- libgen.py
+++ libgen.py
@@ -2,11 +2,12 @@
 # Scan local directory for txt lyric files and put them into lyricLibrary.json
 # The first line is assumed to be the song name, the 2nd to be a space, and 
 # the rest is assumed to be lyrics.
-# Duplicate song names are not allowed.
+
+# If lyricLibrary.json exists duplicate song names won't be overwritten.
 
 import os, glob, json
 
-llf = "../lyricLibrary.json"
+llf = "../lyricLibrary.json" # Where do you want it placed?
 
 localLibrary = {}
 re = "*.txt"
@@ -35,17 +36,24 @@ for fName in matchList:
 if len( localLibrary ) == 0:
   print( "No songs in local directory." )
 else:
-  # see if lyricLibrary.json exists. If so just merge so we don't lose any additional 
-  # song information like key, Artist, etc we may have added.
   if os.path.isfile( llf ):
-    print( "Merging to " + llf )
+    print( "Merging new local songs to " + llf )
     with open( llf ) as jFile:
       existingLibrary = json.load( jFile )
       newCount = 0
-      for key in localLibrary:
-        if key not in existingLibrary:
+      duplicateCount = 0 # songs with the same name locally and in the existing library.
+      for llkey in localLibrary:
+        duplicate = False
+        for elkey in existingLibrary:
+          if elkey.split( "." )[ 0 ] == llkey.split( "." )[ 0 ]:
+            duplicate = True
+            duplicateCount += 1
+            break
+          
+        if not duplicate:
+          print( "Adding:" + llkey.split( "." )[ 0 ] )
           newCount += 1
-          existingLibrary[ key ] = localLibrary[ key ]
+          existingLibrary[ llkey ] = localLibrary[ llkey ]
 
       if newCount:
         with open( llf, 'w' ) as f:
@@ -53,6 +61,8 @@ else:
         print( "added " + str( newCount ) + " songs." )
       else:
         print( "No changes." )
+      print( "There were " + str( duplicateCount) + " duplicates." )
+      
   else:
     print( "Creating " + llf )
     with open( llf, 'w' ) as f:
